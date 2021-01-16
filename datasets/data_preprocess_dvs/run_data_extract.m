7411812cbec4ff7eaa62c0ced8e7bf6ca2a42874
Laura R Edmondson <lredmondson1@sheffield.ac.uk>
2021-01-16 16:46:05 +0000

    Data preprocessing for DVS
    Update data preprocessing steps for DVS data

--- datasets/data_preprocess_dvs/run_data_extract.m
+++ datasets/data_preprocess_dvs/run_data_extract.m
@@ -2,7 +2,7 @@
 % 
 % Preprocessing for CIFAR10DVS, adapted from code for "Convolutional spiking
 % neural networks (SNN) for spatio-temporal feature extraction" paper 
-% Samadzadeh et al.
+% Ali Samadzadeh et al.
 % https://github.com/aa-samad/conv_snn
 
 clc
@@ -10,16 +10,21 @@ clear
 
 labels = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}; % labels
 
-data_path = 'E:\\CIFAR10-DVS\\cifar-raw\\raw'; % update to own datapath
-dest_path = 'dvs-cifar10'; % add save destination path
+data_split = 699;
+total_data = 999;
 
 for folder0 = 1:length(labels)
-    for file0 = 0:999
-        if mod(file0, 10) == 0
-           fprintf('step: %s %d\n', labels{folder0}, file0)
-        end
-        addr = sprintf('%\\%s\\cifar10_%s_%d.aedat', data_path, labels{folder0}, labels{folder0}, file0);
+    % train data
+    for file0 = 0:data_split
+        addr = sprintf('E:\\CIFAR10-DVS\\cifar-raw\\raw\\%s\\cifar10_%s_%d.aedat', labels{folder0}, labels{folder0}, file0);
         out1 = dat2mat(addr);
-        save(sprintf('%\\%s\\%d.mat', dest_path, labels{folder0}, file0), 'out1')
+        save(sprintf('E:\\CIFAR10-DVS\\dvs-cifar10\\train\\%s\\%d.mat', labels{folder0}, file0), 'out1')
+    end
+    
+    % test data
+    for file0 = data_split+1:total_data
+        addr = sprintf('E:\\CIFAR10-DVS\\cifar-raw\\raw\\%s\\cifar10_%s_%d.aedat', labels{folder0}, labels{folder0}, file0);
+        out1 = dat2mat(addr);
+        save(sprintf('E:\\CIFAR10-DVS\\dvs-cifar10\\test\\%s\\%d.mat', labels{folder0}, file0), 'out1')
     end
 end
--- datasets/loadCIFAR10dvs.py
+++ datasets/loadCIFAR10dvs.py
@@ -1,51 +1,86 @@
-'''
-     Preprocessing for CIFAR10DVS, adapted from code for "Convolutional spiking
-     neural networks (SNN) for spatio-temporal feature extraction" paper, 
-     Samadzadeh et al.
-     https://github.com/aa-samad/conv_snn
-'''
-
+# Preprocessing for CIFAR10DVS, adapted from code for "Convolutional spiking
+# neural networks (SNN) for spatio-temporal feature extraction" paper 
+# Ali Samadzadeh et al.
+# https://github.com/aa-samad/conv_snn
 
 import os
 import torch
 from torch.utils.data import Dataset
+import numpy as np
+from os import listdir
+from os.path import isfile
+from scipy.io import loadmat
+
 
 class DVSCifar10(Dataset):
-    def __init__(self, root, train=True, transform=None, target_transform=None):
-        self.root = os.path.expanduser(root)
+    def __init__(self, dataset_path, n_steps, transform=None):
+        self.path = dataset_path
+        self.samples = []
+        self.labels = []
         self.transform = transform
-        self.target_transform = target_transform
-        self.train = train
+        self.n_steps = n_steps
+        
+        mapping = { 0 :'airplane'  ,
+            1 :'automobile',
+            2 :'bird'  ,
+            3 :'cat'   ,
+            4 :'deer'  ,
+            5 :'dog'   ,
+            6 :'frog'  ,
+            7 :'horse' ,
+            8 :'ship'  ,
+            9 :'truck'     }
+
+        for class0 in mapping.keys():
+            sample_dir = dataset_path + mapping[class0] + '/'
+            for f in listdir(sample_dir):
+                filename = sample_dir + "{}".format(f)
+                if isfile(filename):
+                    self.samples.append(filename)
+                    self.labels.append(class0)
 
     def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-        data, target = torch.load(self.root + '{}.pt'.format(index))
+        filename = self.samples[index]
+        label = self.labels[index]
 
-        if self.transform is not None:
+        events = loadmat(filename)['out1']
+        data = np.zeros((2, 128, 128, self.n_steps))
+        
+        # --- building time surfaces
+        for i in range(self.n_steps): # frames
+            r1 = i * (events.shape[0] // self.n_steps)
+            r2 = (i + 1) * (events.shape[0] // self.n_steps) # split into 10 frames
+            data[events[r1:r2, 3], events[r1:r2, 1], events[r1:r2, 2], i] += events[r1:r2, 0] # add each frame
+        
+        for i in range(10): # normalise across each time frame?
+            data[:, :, :, i] = data[:, :, :, i] / np.max(data[:, :, :, i])
+            
+        if self.transform:
             data = self.transform(data)
+            data = data.type(torch.float32)
+        else:
+            data = torch.FloatTensor(data)
 
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return data, target.long()
+        return data, label
 
     def __len__(self):
-        return len(os.listdir(self.root))
+        return len(self.samples)
 
 
 def get_cifar10dvs(data_path, network_config):
-    print("loading CIFAR10-DVS")
+    n_steps = network_config['n_steps']
     batch_size = network_config['batch_size']
+    print("loading CIFAR10-DVS")
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+        
+    train_path = data_path + '/dvs-cifar10/train/'
+    test_path = data_path + '/dvs-cifar10/test/'
+    
+    trainset = DVSCifar10(train_path, n_steps)
+    testset = DVSCifar10(test_path, n_steps)
 
-    trainset = data_path + '/dvs-cifar10/train/'
-    trainloader = torch.utils.data.DataLoader(DVSCifar10(trainset), batch_size=batch_size, shuffle=True, num_workers=8)
-
-    testset = data_path + '/dvs-cifar10/test/'
-    testloader = torch.utils.data.DataLoader(DVSCifar10(testset), batch_size=batch_size, shuffle=False, num_workers=8)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4)
 
     return trainloader, testloader
